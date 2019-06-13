c0f7f57905710c7efbd64619025cbcfffac287ff
cbelgrain <cecilia.belgrain@gmail.com>
2019-06-13 16:20:02 +0200

    first commit nav

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="search">
+    <input type="search" name="search" id="search" placeholder="Recherche" />
+    <input type="submit" value="Entrer">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchInput'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/sections/Navigation.vue
+++ src/components/sections/Navigation.vue
@@ -1,15 +1,48 @@
 <template>
   <div class="nav">
-    <p>navigation a mettre en forme</p>
+    <div class="nav_site">
+      <div class="nav_site_img">
+        <img src="../../assets/img/logo.jpg" alt="logo">
+      </div>
+      <h1>SuperComics</h1>
+    </div>
+    <search-input />
   </div>
 </template>
 
 <script>
+import SearchInput from '../SearchInput'
+
 export default {
-  name: 'Navigation'
+  name: 'Navigation',
+  components: {
+    SearchInput
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  .nav {
+    height: 100px;
+    border-bottom: 1px solid;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
 
+    &_site {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      width: 50%;
+
+      &_img {
+        width: 100px;
+
+        & img {
+          width: 100%
+        }
+      }
+    }
+  }
 </style>
