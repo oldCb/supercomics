9430f7209179c8f6ca2cf90b9e9fcbe4b58b7dec
cbelgrain <cecilia.belgrain@gmail.com>
2019-06-13 16:46:45 +0200

    fix css app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <component :is="nav">
       <router-view />
     </component>
-    <router-view class="page" />
+    <router-view class="app_page" />
     <component :is="footer">
       <router-view />
     </component>
@@ -27,7 +27,10 @@ export default {
 </script>
 
 <style lang="scss">
-  .page {
-    margin-bottom: 50px;
+  #app {
+
+    &_page {
+      margin-bottom: 50px;
+    }
   }
 </style>
