092de8f67c0b8a6ff1e01095e3add5a71d4ac731
cbelgrain <cecilia.belgrain@gmail.com>
2019-06-17 11:14:22 +0200

    components for hero page

---
+++ src/components/HeroAnimationFilms.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hero_animation_films">
+    <h2>Films d'animation</h2>
+    <ul>
+      <li>Lorem ipsum</li>
+      <li>Lorem ipsum</li>
+      <li>Lorem ipsum</li>
+      <li>Lorem ipsum</li>
+      <li>Lorem ipsum</li>
+      <li>Lorem ipsum</li>
+      <li>Lorem ipsum</li>
+      <li>Lorem ipsum</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroAnimationFilms'
+}
+</script>
+
+<style lang="scss" scoped>
+  .hero_animation_films {
+    width: 25%;
+    margin: 50px 0;
+
+    & h2 {
+      text-align: center;
+    }
+
+    & ul {
+      list-style: none;
+      padding-left: 30px;
+    }
+  }
+</style>
---
+++ src/components/HeroSlider.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="slider">
+    <img src="../assets/img/spiderman/spiderman1.jpg" alt="spiderman">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroSlider'
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .slider  {
+    text-align: center;
+
+    & img {
+      width: 60%;
+    }
+  }
+</style>
