b419aebf73395c9f1c498dfcb5d62112b6113804
jordannlr <jordan.neller@gmail.com>
2019-06-17 15:47:50 +0200

    save

---
+++ src/components/SuperHeros.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="listeHeros">
+      <div class="superH" v-for="superhero in superheros" :key="superhero.id">
+
+          <img src="">
+          <div class="description">
+              <p>{{superhero.title}}</p>
+              <p>{{superhero.description}}</p>
+              </div>
+          
+          
+
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SuperHeros',
+data () {
+    return {
+      superheros: [
+      { img:'/img/blackpanther.jpg', title: 'Black Panther', description: 'Black Panther est un super-héros évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics.' },
+      { img:'/img/captainamerica.jpg', title: 'Captain America', description: 'Captain America est un super-héros de bande dessinée évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics.' },
+      { img:'/img/batman.jpg', title: 'Batman', description: 'Bruce Wayne, alias Batman, est un héros de fiction appartenant à l\'univers de DC Comics.' }
+    ]
+  }
+}
+}
+
+</script>
+
+<style>
+    .listeHeros {
+        margin: auto;
+        margin-top: 50px;
+        margin-bottom: 50px;
+        width: 900px;
+        height: 900px;
+        border: solid;
+    }
+
+    .superH {
+        border: 1px solid;
+        width: 300px;
+        height: 300px;
+
+    }
+
+
+    .superH img {
+        filter: grayscale(1);
+        -webkit-filter: grayscale(1);
+    }
+    .superH img:hover {
+        filter: grayscale(0);
+        -webkit-filter: grayscale(0);
+    }
+    
+</style>
