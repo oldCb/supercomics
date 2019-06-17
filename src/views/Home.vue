63b2dae1ad2f4830b924e9bb73fe6bf294ae8e84
jordannlr <jordan.neller@gmail.com>
2019-06-17 15:07:55 +0200

    superheros

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <home-header/>
+    <the-button/>
+    <super-heros/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeHeader from '@/components/HomeHeader.vue'
+import TheButton from '@/components/TheButton.vue'
+import SuperHeros from '@/components/SuperHeros.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HomeHeader,
+    TheButton,
+    SuperHeros
   }
 }
 </script>
